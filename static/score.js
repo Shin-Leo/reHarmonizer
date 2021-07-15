3abc5d1d5016c19490ee1316b03fd2becb76e425
Leo Shin <54517654+Shin-Leo@users.noreply.github.com>
2021-07-15 15:31:37 -0700

    Adds a new instance of the svg with notes added

--- static/score.js
+++ static/score.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     VF = Vex.Flow;
 
-    var div = document.getElementById("boo")
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    let div = document.getElementById("boo")
+    let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
     renderer.resize(500, 500);
 
-    var context = renderer.getContext();
-    var stave = new VF.Stave(10, 40, 400);
+    let context = renderer.getContext();
+    let stave = new VF.Stave(10, 40, 400);
 
     stave.addClef("treble").addTimeSignature("4/4");
 
@@ -18,6 +18,7 @@ $(document).ready(function () {
         getMousePosition(e, pt)
     })
 })
+
 function addQuarterNote() {
 
 }
@@ -34,16 +35,41 @@ function cursorPoint(evt, pt) {
 }
 
 function drawNote() {
+    VF = Vex.Flow;
+
+    let div = document.getElementById("boo")
+    let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+    renderer.resize(500, 500);
+
+    let context = renderer.getContext();
+
     let notes = [
-        new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "q"}),
-    ]
+        new VF.StaveNote({clef: "treble", keys: ["c/5"], duration: "q"}),
+        new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q"}),
+        new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr"}),
+        new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q"})
+    ];
 
-    var voice = new VF.Voice({num_beats: 4, beat_value: 4});
-    voice.addTickables(notes);
 
-    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+// Create a voice in 4/4 and add the notes from above
+    let voices = [
+        new VF.Voice({num_beats: 4, beat_value: 4}).addTickables(notes)
+]
+// Format and justify the notes to 400 pixels.
+    let formatter = new VF.Formatter().joinVoices(voices).format(voices, 400);
 
-    voice.draw(context, stave);
-        let div = document.getElementById("boo")
+// Render voices
+
+    let stave = new VF.Stave(10, 40, 400);
+
+    stave.addClef("treble").addTimeSignature("4/4");
+
+    stave.setContext(context).draw();
+    let svg = document.querySelector("#boo > svg")
+
+    voices.forEach(function (v) {
+        v.draw(context, stave);
+    })
 }
 
