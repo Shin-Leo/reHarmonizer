0bdebf329cafb2895fe31baccc5beb9a07f921cf
Leo Shin <54517654+Shin-Leo@users.noreply.github.com>
2021-07-15 21:08:11 -0700

    added detection and spacing for quarter note projections

--- static/score.js
+++ static/score.js
@@ -1,23 +1,34 @@
-function cursorPoint(evt) {
-    pt.x = evt.clientX;
-    pt.y = evt.clientY;
-    return pt.matrixTransform(svg.getScreenCTM().inverse());
-}
-
-
-function projectNote(rect, coordinates, svg) {
-    document.getElementById("note-projection").setAttribute("cx", coordinates.x)
-    document.getElementById("note-projection").setAttribute("cy", coordinates.y)
-    console.log(document.getElementById("note-projection").attributes)
+function projectNote(rect, coordinates, availableWidth) {
+    let start = parseInt(rect[1].value)
+    let inc = parseInt(availableWidth)/4
+    let xPos = 0
+    if (coordinates.x >= start && coordinates.x <= start + inc) {
+        xPos= start + (inc/ 2)
+        document.getElementById("note-projection").setAttribute("cx", String(xPos))
+        document.getElementById("note-projection").setAttribute("cy",String(rect[2].value-2.5))
+    } else if (coordinates.x > start+inc && coordinates.x <= start + inc * 2) {
+        xPos= start + (inc*2)*(3/4)
+        document.getElementById("note-projection").setAttribute("cx", String(xPos))
+        document.getElementById("note-projection").setAttribute("cy",String(rect[2].value-2.5))
+    } else if (coordinates.x > start+inc * 2 && coordinates.x <= start + inc * 3) {
+        xPos= start +((inc*3)*(5/6))
+        document.getElementById("note-projection").setAttribute("cx", String(xPos))
+        document.getElementById("note-projection").setAttribute("cy",String(rect[2].value-2.5))
+    } else if (coordinates.x > start+inc * 3 && coordinates.x <= start + inc * 4) {
+        xPos= start +(inc*4)*(7/8)
+        document.getElementById("note-projection").setAttribute("cx", String(xPos))
+        document.getElementById("note-projection").setAttribute("cy",String(rect[2].value-2.5))
+    }
 }
 
 $(document).ready(function () {
     VF = Vex.Flow;
-
     let div = document.getElementById("boo")
     let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    let rendererHeight = 500
+    let rendererWidth  = 200
 
-    renderer.resize(500, 200);
+    renderer.resize(rendererHeight, rendererWidth);
 
     let context = renderer.getContext();
     let staveX = 400
@@ -41,34 +52,37 @@ $(document).ready(function () {
             "            </rect>`
         let xRectangleHtml;
         if (i % 2) {
-            xRectangleHtml = `<rect id=\"${"xRectangleHtml" + id}\" x=\"${staveX * 0.2}\" y=\"${xRectHeight + 1.36 * staveY}\" width=\"300\" height=\"6\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n" +
-            "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
-            "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
+            xRectangleHtml = `<rect id=\"${"xRectangleHtml" + id}\" x=\"${staveX * 0.2}\" y=\"${xRectHeight + 1.36 * staveY}\" width=\"300\" height=\"6\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n"
+ +
+<!--            "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +-->
+<!--            "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +-->
             "            </rect>`
             xRectHeight += 6
         } else {
-            xRectangleHtml = `<rect id=\"${"xRectangleHtml" + id}\" x=\"${staveX * 0.2}\" y=\"${xRectHeight + 1.36 * staveY}\" width=\"300\" height=\"4\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n" +
-            "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
-            "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
+            xRectangleHtml = `<rect id=\"${"xRectangleHtml" + id}\" x=\"${staveX * 0.2}\" y=\"${xRectHeight + 1.36 * staveY}\" width=\"300\" height=\"4\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n" 
++
+<!--            "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +-->
+<!--            "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +-->
             "            </rect>`
             xRectHeight += 4
         }
 
-        svg.insertAdjacentHTML('afterbegin', yRectangleHtml)
-        // svg.insertAdjacentHTML('beforeend', xRectangleHtml)
+        // svg.insertAdjacentHTML('beforeennd', yRectangleHtml)
+        svg.insertAdjacentHTML('beforeend', xRectangleHtml)
         yRectWidth += 20
         let yRectangle = document.getElementById("yRectangleHtml" + id)
-        let xRectangle = document.getElementById("yRectangleHtml" + id)
+        let xRectangle = document.getElementById("xRectangleHtml" + id)
         xRectangles.push(xRectangle)
-        yRectangles.push(yRectangle)
+        // yRectangles.push(yRectangle)
         id += 1
     }
-    svg.insertAdjacentHTML('beforeend', `<ellipse id="note-projection" cx=\"400\" cy=\"400\" rx=\"4\" ry=\"3\" opacity=\"1\">`)
+    svg.insertAdjacentHTML('beforeend', `<ellipse id="note-projection" cx=\"400\" cy=\"400\" rx=\"5\" ry=\"4\" opacity=\"1\">`)
     let pt = svg.createSVGPoint();
-    yRectangles.forEach((rect) => {
+
+
+    xRectangles.forEach((rect) => {
         rect.addEventListener("mousemove", function (e) {
-            pt.x = e.target.attribute
-            projectNote(rect, getMousePosition(e, pt), svg)
+            projectNote(e.target.attributes, getMousePosition(e, pt), staveX-e.target.attributes[1].value)
         })
     })
     svg.addEventListener("click", function (e) {
@@ -76,13 +90,19 @@ $(document).ready(function () {
     })
 })
 
+
 function addQuarterNote() {
 
 }
 
+function cursorPoint(evt, pt) {
+    pt.x = evt.clientX;
+    pt.y = evt.clientY;
+    return pt;
+}
+
 function getMousePosition(evt, pt) {
     let cursorPt = cursorPoint(evt, pt);
-    console.log("(" + cursorPt.x + ", " + cursorPt.y + ")");
     return cursorPt
 }
 
