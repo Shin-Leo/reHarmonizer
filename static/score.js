80cbe13518f84895c121cd370a2595945c580947
Leo Shin <54517654+Shin-Leo@users.noreply.github.com>
2021-07-15 19:53:04 -0700

    added a note projection based on user mouse movement

--- static/score.js
+++ static/score.js
@@ -1,3 +1,16 @@
+function cursorPoint(evt) {
+    pt.x = evt.clientX;
+    pt.y = evt.clientY;
+    return pt.matrixTransform(svg.getScreenCTM().inverse());
+}
+
+
+function projectNote(rect, coordinates, svg) {
+    document.getElementById("note-projection").setAttribute("cx", coordinates.x)
+    document.getElementById("note-projection").setAttribute("cy", coordinates.y)
+    console.log(document.getElementById("note-projection").attributes)
+}
+
 $(document).ready(function () {
     VF = Vex.Flow;
 
@@ -7,23 +20,57 @@ $(document).ready(function () {
     renderer.resize(500, 200);
 
     let context = renderer.getContext();
-    let staveWidth = 400
-    let stave = new VF.Stave(10, 40, staveWidth);
+    let staveX = 400
+    let staveY = 50
+    let stave = new VF.Stave(10, staveY, staveX);
 
     stave.addClef("treble").addTimeSignature("4/4");
 
     stave.setContext(context).draw();
     let svg = document.querySelector("#boo > svg")
-    let rectWidth = 0
+    let yRectWidth = 0
+    let xRectHeight = 0
+    let id = 1
+    let yRectangles = []
+    let xRectangles = []
     for (let i = 0; i < 16; i++) {
-            let rectangleHtml = `<rect x=\"${rectWidth + staveWidth * 0.2}\" y=\"20\" width=\"20\" height=\"100\" style=\"fill:rgb(183,241,222);\" opacity=\"0\" onmouseover=\"set_opacity(evt, 1);\" onmouseout=\"set_opacity(evt, 0);\">\n" +
+        let yRectangleHtml = `<rect id=\"${"yRectangleHtml" + id}\" x=\"${yRectWidth + staveX * 0.2}\" y=\"50\" width=\"20\" height=\"100\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n" 
++
+<!--            "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +-->
+<!--            "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +-->
+            "            </rect>`
+        let xRectangleHtml;
+        if (i % 2) {
+            xRectangleHtml = `<rect id=\"${"xRectangleHtml" + id}\" x=\"${staveX * 0.2}\" y=\"${xRectHeight + 1.36 * staveY}\" width=\"300\" height=\"6\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n" +
             "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
             "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
             "            </rect>`
-        svg.insertAdjacentHTML('beforeend', rectangleHtml)
-        rectWidth += 20
+            xRectHeight += 6
+        } else {
+            xRectangleHtml = `<rect id=\"${"xRectangleHtml" + id}\" x=\"${staveX * 0.2}\" y=\"${xRectHeight + 1.36 * staveY}\" width=\"300\" height=\"4\" style=\"fill:rgb(183,241,222);\" opacity=\"0\">\n" +
+            "              <animate attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"mouseover\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
+            "              <animate attributeName=\"opacity\" to=\"0\" begin=\"mouseout\" dur=\"0.5s\" fill=\"freeze\"/>\n" +
+            "            </rect>`
+            xRectHeight += 4
+        }
+
+        svg.insertAdjacentHTML('afterbegin', yRectangleHtml)
+        // svg.insertAdjacentHTML('beforeend', xRectangleHtml)
+        yRectWidth += 20
+        let yRectangle = document.getElementById("yRectangleHtml" + id)
+        let xRectangle = document.getElementById("yRectangleHtml" + id)
+        xRectangles.push(xRectangle)
+        yRectangles.push(yRectangle)
+        id += 1
     }
+    svg.insertAdjacentHTML('beforeend', `<ellipse id="note-projection" cx=\"400\" cy=\"400\" rx=\"4\" ry=\"3\" opacity=\"1\">`)
     let pt = svg.createSVGPoint();
+    yRectangles.forEach((rect) => {
+        rect.addEventListener("mousemove", function (e) {
+            pt.x = e.target.attribute
+            projectNote(rect, getMousePosition(e, pt), svg)
+        })
+    })
     svg.addEventListener("click", function (e) {
         getMousePosition(e, pt)
     })
@@ -34,8 +81,9 @@ function addQuarterNote() {
 }
 
 function getMousePosition(evt, pt) {
-    let cursorpt = cursorPoint(evt, pt);
-    console.log("(" + cursorpt.x + ", " + cursorpt.y + ")");
+    let cursorPt = cursorPoint(evt, pt);
+    console.log("(" + cursorPt.x + ", " + cursorPt.y + ")");
+    return cursorPt
 }
 
 function cursorPoint(evt, pt) {
@@ -65,7 +113,7 @@ function drawNote() {
 // Create a voice in 4/4 and add the notes from above
     let voices = [
         new VF.Voice({num_beats: 4, beat_value: 4}).addTickables(notes)
-]
+    ]
 // Format and justify the notes to 400 pixels.
     let formatter = new VF.Formatter().joinVoices(voices).format(voices, 400);
 
